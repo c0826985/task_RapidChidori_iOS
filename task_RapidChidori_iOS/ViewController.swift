2a29b9d648315f7c2a64a1347512ea07c7f02c1a
Kaushil <backs.kaushil@gmail.com>
2022-01-13 18:11:04 -0500

    Initial Commit

---
+++ task_RapidChidori_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_RapidChidori_iOS
+//
+//  Created by Kaushil Prajapati on 13/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
